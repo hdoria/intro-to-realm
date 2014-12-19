5d460bdbd4f978d336b3c8f84af0b54c0fb20852
Hugo Doria <hugo@popcode.com.br>
2014-12-19 12:22:23 -0300

    new xcode project :  introduction to realm

---
+++ Introduction to Realm/Introduction to Realm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Introduction to Realm
+//
+//  Created by Hugo Doria on 19/12/14.
+//  Copyright (c) 2014 Hugo Doria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
