57df9223b155a42612da8b03d2c6199a26aaf241
Hugo Doria <hugo@popcode.com.br>
2014-12-19 12:40:02 -0300

    creating our first model: artist

---
+++ Introduction to Realm/Introduction to Realm/Models/Artist.swift
@@ -0,0 +1,18 @@
+//
+//  Artist.swift
+//  Introduction to Realm
+//
+//  Created by Hugo Doria on 19/12/14.
+//  Copyright (c) 2014 Hugo Doria. All rights reserved.
+//
+
+import Realm
+
+class Artist: RLMObject {
+    dynamic var name = ""
+    dynamic var contry = ""
+    
+    override class func primaryKey() -> String! {
+        return "name"
+    }
+}
